e58b50b6e2cb1a71b58722cb7505340c2406f700
Dan Feblowitz <dan@blockpower.vote>
2020-11-23 16:58:33 -0500

    175827416: add edit profile feature, clean up CardVolunteerFull

--- src/components/Volunteers/CardVolunteerFull.jsx
+++ src/components/Volunteers/CardVolunteerFull.jsx
@@ -1,26 +1,16 @@
 import React, { Component } from "react";
 
-import { geocodeByAddress, getLatLng } from "react-places-autocomplete";
-import Select from "react-select";
-
 import Button from "@material-ui/core/Button";
-import List from "@material-ui/core/List";
 
-import {
-  notify_error,
-  notify_success,
-  _fetch,
-  PlacesAutocomplete,
-} from "../../common.js";
+import { fetch } from "./../common.js";
 
-import { CardTurf } from "../Turf";
-import { CardForm } from "../Forms";
+import VolunteerInfo from "./VolunteerInfo";
 
 import TimeAgo from "javascript-time-ago";
 import en from "javascript-time-ago/locale/en";
+
 TimeAgo.locale(en);
 
-const NEARBY_DIST = 50;
 export const CardVolunteerFull = (props) => (
   <div>
     <div>Has W9?: {props.volunteer.has_w9 ? "Yes" : "No"}</div>
@@ -53,16 +43,9 @@ export const CardVolunteerFull = (props) => (
         Make Admin
       </Button>
     </div>
-    Email: {props.volunteer.email ? props.volunteer.email : "N/A"}
-    <br />
-    Phone: {props.volunteer.phone ? props.volunteer.phone : "N/A"}
-    <br />
-    Address:
-    <VolunteerAddress
-      global={global}
-      refer={props.refer}
-      volunteer={props.volunteer}
-    />
+
+    <VolunteerInfo volunteer={props.volunteer} global={props.global} reload={props.refer._loadData}/>
+
     {props.volunteer.verification ? (
       <div>{JSON.stringify(props.volunteer.verification, null, 2)}</div>
     ) : (
@@ -99,50 +82,3 @@ export const CardVolunteerFull = (props) => (
     <br />
   </div>
 );
-
-export class VolunteerAddress extends Component {
-  constructor(props) {
-    super(props);
-    let addr = this.props.volunteer.address;
-    this.state = {
-      global: props.global,
-      edit: false,
-      address: `${addr.address1} ${addr.city}, ${addr.state} ${addr.zip}`,
-    };
-    this.onTypeAddress = (address) => this.setState({ address });
-  }
-
-  submitAddress = async (address) => {
-    const { global } = this.state;
-
-    this.setState({ address });
-    try {
-      let res = await geocodeByAddress(address);
-      let pos = await getLatLng(res[0]);
-      await _fetch(global, "/volunteer/update", "POST", {
-        id: this.props.volunteer.id,
-        address: address,
-        lat: pos.lat,
-        lng: pos.lng,
-      });
-      this.props.refer._loadData();
-      notify_success("Address hass been saved.");
-    } catch (e) {
-      notify_error(e, "Unable to update address info.");
-    }
-  };
-
-  render() {
-    if (this.state.edit)
-      return (
-        <PlacesAutocomplete
-          debounce={500}
-          value={this.state.address}
-          onChange={this.onTypeAddress}
-          onSelect={this.submitAddress}
-        />
-      );
-
-    return <div>{this.state.address} </div>;
-  }
-}
