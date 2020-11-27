8062d731cedd117090b7a0f13aa0d1ae017b948a
Cristina <ce@tsl.io>
2020-11-27 18:26:46 -0500

    removing extra stuff

--- src/components/Volunteers/VolunteerTriplerInteraction.jsx
+++ src/components/Volunteers/VolunteerTriplerInteraction.jsx
@@ -1,34 +1,12 @@
 import React, { Component } from "react";
-
-import { HashRouter as Router, Route, Link } from "react-router-dom";
-import Select from "react-select";
-
-import Modal from "@material-ui/core/Modal";
 import List from "@material-ui/core/List";
-import Button from "@material-ui/core/Button";
-
-import {
-  notify_error,
-  _fetch,
-  _searchStringify,
-  _loadTriplers,
-  RootLoader,
-  DialogSaving,
-  InviteSomeone,
-} from "../../common.js";
-
 import { CardTripler } from "../Triplers/CardTripler";
-
-import TimeAgo from "javascript-time-ago";
-import en from "javascript-time-ago/locale/en";
-TimeAgo.locale(en);
 
 export default class App extends Component {
   constructor(props) {
     console.log(props.volunteer);
     super(props);
     this.state = {
-      global: props.global,
       loading: true,
       thisTripler: {},
       triplers: props.volunteer.claimees,
@@ -42,24 +20,11 @@ export default class App extends Component {
   }
 
   _loadData = async () => {
-    const { global } = this.state;
-
-    let triplers = [];
     this.setState({ loading: false, firstName: "", lastName: "" });
-  };
-
-  handlePageClick = (data) => {
-    this.setState({ pageNum: data.selected + 1 });
   };
 
   render() {
     const { global } = this.state;
-
-    let ready = [];
-    let unassigned = [];
-    let incomplete = [];
-    let denied = [];
-    let invited = [];
 
     return (
       <div flag={this.state.loading} func={() => this._loadData()}>
